0d76b474b1fcf2dda4ff5d6118e903cf653de70f
1-ПК\1 <anastasiatsvetkova1995@mail.ru>
2017-04-05 00:12:59 +0300

    no message

---
+++ CSharpCourse/HomeWork5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    abstract class Detail
+    {
+        public abstract string Weight { get; set; }
+        public abstract string Name { get; set; }
+    }
+    public class Car : Detail { get; set Model; }
+    {    
+        public Wheel {get Number; set}
+        {
+         interface IRotatable
+         {
+         void Move();
+         }
+        private int numberofWheels;
+        return numberWheel;
+        
+        Console.WriteLine("Колесо № {0} машины {1} вращается");
+        class Door
+        {
+    private int numberOfDoors;
+    interface IDoor
+    {
+        void Open();
+        Console.Write("Дверь № {0} машины {1} ");
+        Console.Write(isOpenDoor? "открыта" : "закрыта");
+        Console.WriteLine();
+    }
+    }
+    class Body
+{
+
+}
+}
+
