48f9965ba668ec3f3eb8a5de652dd59f8c486927
1-ПК\1 <anastasiatsvetkova1995@mail.ru>
2017-04-09 09:18:41 +0300

    HomeWork6

---
+++ CSharpCourse/HomeWork6/Matrix.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6
+{
+        public class Matrix
+        {
+            private int numberLines;
+            private int numberColumns;
+            public int[,] matrix;
+            Random ran = new Random((int)DateTime.Now.Ticks);
+            public Matrix(int numberLines, int numberColumns)
+            {
+                this.numberLines = numberLines;
+                this.numberColumns = numberColumns;
+                matrix = primerMatrix();
+            }
+            private int[,] primerMatrix()
+            {
+                int[,] array = new int[numberLines, numberColumns];
+
+                for (int i = 0; i < numberLines; i++)
+                {
+                    for (int j = 0; j < numberColumns; j++)
+                    {
+                        array[i, j] = ran.Next(-100, 100);
+                    }
+                }
+                return array;
+            }
+
+            public int[,] Sum(int[,] matrix2)
+            {
+                int[,] arrayResult = new int[numberLines, numberColumns];
+
+                for (int i = 0; i < numberLines; i++)
+                {
+                    for (int j = 0; i < numberColumns; j++)
+                    {
+                        arrayResult[i, j] = matrix[i, j] + matrix2[i, j];
+                    }
+                }
+                return arrayResult;
+            }
+            //public int[,] SumWithNumber(int number) - ?невозможно сложить матрицу с числом?
+            //{
+            //    int[,] arraySumWithNumber = new int[numberLines, numberColumns];
+
+            //    for (int i = 0; i < numberLines; i++)
+            //    {
+            //        for (int j = 0; i < numberColumns; j++)
+            //        {
+            //            arraySumWithNumber[i, j] = matrix[i, j] + number;
+            //        }
+            //    }
+            //    return arraySumWithNumber;
+            //}
+
+            public int[,] Multiply(int[,] matrix3)
+            {
+                int[,] arrayMultiply = new int[numberLines, numberColumns];
+                for (int i = 0; i < numberLines; i++)
+                {
+                    for (int j = 0; i < numberColumns; j++)
+                    {
+                        for (int r = 0; i < numberColumns; i++)
+                        {
+                            arrayMultiply[i, j] += matrix[i, r] * matrix3[r, j];
+                        }
+                    }
+                }
+                return arrayMultiply;
+
+            }
+            public int[,] MultiplyOnNumber(int number)
+            {
+                for (int i = 0; i < numberLines; i++)
+                {
+                    for (int j = 0; i < numberColumns; j++)
+                    {
+                        matrix[i, j] = matrix[i, j] * number;
+                    }
+                }
+                return matrix;
+            }
+            public int[,] Transpose()
+            {
+                int[,] trans = new int[numberLines, numberColumns];
+
+                for (int i = 0; i < numberLines; i++)
+                {
+                    for (int j = 0; j < numberColumns; j++)
+                    {
+                        trans[i, j] = matrix[j, i];
+                    }
+                }
+                return matrix;
+            }
+        }
+    }
+
---
+++ CSharpCourse/HomeWork6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк для матрицы");
+            int numberOfLines = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов для матрицы");
+            int numberOfColumns = int.Parse(Console.ReadLine());
+
+            Matrix matr1 = new Matrix(numberOfLines, numberOfColumns);
+            Matrix matr2 = new Matrix(numberOfLines, numberOfColumns);
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                for (int j = 0; j < numberOfColumns; j++)
+                {
+                    Console.Write(matr1.matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            int[,] resultsumMatr1andMatr2 = matr1.Sum(matr2.matrix);
+            Console.WriteLine();
+
+            Console.WriteLine("Введите число для его сложения с матрицей");
+            int number = int.Parse(Console.ReadLine());
+
+            int[,] multiplyOnNumber = matr1.matrix;
+
+
+        }
+    }
+}
+
